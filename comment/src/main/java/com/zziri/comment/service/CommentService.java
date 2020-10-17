61abd28f6f3a65359f2fa94244755d447d2a93dd
zziri <jihzang@naver.com>
2020-10-18 04:03:47 +0900

    CommentService: 저장할 때 서버 기준 시간을 저장하도록 변경

--- comment/src/main/java/com/zziri/comment/service/CommentService.java
+++ comment/src/main/java/com/zziri/comment/service/CommentService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Transactional
@@ -16,6 +17,7 @@ public class CommentService {
     private CommentRepository commentRepository;
 
     public Comment put(CommentDto commentDto) {
+        commentDto.setDate(LocalDateTime.now());
         return commentRepository.save(Comment.fromDto(commentDto));
     }
 
