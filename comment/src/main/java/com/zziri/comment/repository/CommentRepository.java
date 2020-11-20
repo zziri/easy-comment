5c2f524b82c8fc3391c100d67bb3b4e19dec3b26
zziri <jihzang@naver.com>
2020-11-20 23:59:45 +0900

    CommentRepository: findByIdHashCode

--- comment/src/main/java/com/zziri/comment/repository/CommentRepository.java
+++ comment/src/main/java/com/zziri/comment/repository/CommentRepository.java
@@ -3,9 +3,13 @@ package com.zziri.comment.repository;
 
 import com.zziri.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByUrl(String url);
+    @Query(value = "select * from comment where comment.id = id and comment.hash_code = hashCode", nativeQuery = true)
+    Comment findByIdHashCode(@Param("id") Long id, @Param("hashCode") String hashCode);
 }
